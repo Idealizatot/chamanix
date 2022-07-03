90ccea98d769a801f02c4ebd94e6e4a1278b251a
ryzhenkov.d <ryzhenkov.d@nakukop.com>
2022-07-03 23:42:54 +0300

    add elsint+prittier

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>123</header>
+      <main>456</main>
+      <footer>789</footer>
+    </>
+  );
+}
+
+export default App;
